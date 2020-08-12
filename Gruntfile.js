a7118e15721f6c7652b552df686bab2188c0ece3
Boris Fritscher <boris@fritscher.ch>
2020-08-12 11:31:10 +0200

    migrate to connect3

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,6 +21,8 @@ module.exports = function (grunt) {
         'string-replace': 'grunt-string-replace'
     });
 
+    var serveStatic = require('serve-static');
+
     // Configurable paths for the application
     var appConfig = {
         app: require('./bower.json').appPath || 'app',
@@ -82,16 +84,16 @@ module.exports = function (grunt) {
                     open: true,
                     middleware: function (connect) {
                         return [
-                            connect.static('.tmp'),
+                            serveStatic('.tmp'),
                             connect().use(
                                 '/bower_components',
-                                connect.static('./bower_components')
+                                serveStatic('./bower_components')
                             ),
                             connect().use(
                                 '/app/styles',
-                                connect.static('./app/styles')
+                                serveStatic('./app/styles')
                             ),
-                            connect.static(appConfig.app)
+                            serveStatic(appConfig.app)
                         ];
                     }
                 }
