46eed46e6e939da2f81c1c6db534169d76bced3c
Boris Fritscher <boris@fritscher.ch>
2021-12-03 10:25:26 +0100

    fix: sed switch delimiter to support DSN @

--- nginx/inject-env.sh
+++ nginx/inject-env.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sed -i 's@$GA_CODE@'"$GA_CODE"'@g' /usr/share/nginx/html/index.html
-sed -i 's@$AMCUI_API@'"$AMCUI_API"'@g' /usr/share/nginx/html/js/app*.js
-sed -i 's@$SENTRY_DSN@'"$SENTRY_DSN"'@g' /usr/share/nginx/html/js/app*.js
+sed -i 's=$GA_CODE='"$GA_CODE"'=g' /usr/share/nginx/html/index.html
+sed -i 's=$AMCUI_API='"$AMCUI_API"'=g' /usr/share/nginx/html/js/app*.js
+sed -i 's=$SENTRY_DSN='"$SENTRY_DSN"'=g' /usr/share/nginx/html/js/app*.js
