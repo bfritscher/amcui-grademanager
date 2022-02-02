e72e659d2ace1e7bd7351658a9537b0a85a52fb5
Boris Fritscher <boris@fritscher.ch>
2022-02-02 21:36:41 +0100

    feat: nicer stats page
    css spacing
    change order for multiple answers, response 0 at the end
    question number to letter by default

--- src/components/Grade/GradeStats.vue
+++ src/components/Grade/GradeStats.vue
@@ -6,7 +6,7 @@
       map-options
       emit-value
       borderless
-      style="width: 200px;"
+      style="width: 200px"
       label-color="primary"
       color="primary"
       label="Sort by"
@@ -14,24 +14,30 @@
     <q-btn flat aria-label="catalog" color="primary" @click="downloadCatalog()"
       >Download&nbsp;<small>Catalog.pdf</small></q-btn
     >
+    <q-checkbox v-model="showLetters" label="Show letters" size="sm" />
   </q-toolbar>
   <div class="row">
     <div v-for="row in sortedStats" :key="row.title" class="question-stat">
       <div class="text-h5 text-bold">{{ row.title }}</div>
       <div>
-        <b>total:</b> {{ row.total }} <b>mean:</b>
-        {{ (row.avg * 100).toFixed(2) }}%
+        Total: <b>{{ row.total }}</b> Mean:
+        <b>{{ (row.avg * 100).toFixed(2) }}%</b>
       </div>
       <div class="answer-dist">
         <div
-          v-for="(a, index) in row.answers"
+          v-for="(a, index) in sortedAnswers(row.answers)"
           :key="index"
           :style="{ width: (a.nb / row.total) * 100 + '%' }"
           class="answer-stat"
-          :class="`correct-${a.correct}`"
+          :class="correctToColor(a.correct)"
         >
-          <span>{{ a.answer }} {{ a.nb }}</span
-          ><span>{{ ((a.nb / row.total) * 100).toFixed(2) }}%</span>
+          <span class="answer-letter">{{
+            toLetter(a.answer, row.answers.length - 2)
+          }}</span>
+          <span class="answer-count">({{ a.nb }})</span>
+          <span class="answer-percent"
+            >{{ ((a.nb / row.total) * 100).toFixed(2) }}%</span
+          >
         </div>
       </div>
     </div>
@@ -52,6 +58,7 @@ export default defineComponent({
   data() {
     return {
       sortBy: 'title',
+      showLetters: true,
       sortItems: [
         {
           value: 'title',
@@ -80,6 +87,38 @@ export default defineComponent({
     downloadCatalog() {
       window.open(this.API.getStaticFileURL('catalog.pdf'));
     },
+    correctToColor(correct: number) {
+      if (correct === 0) {
+        return 'bg-warning';
+      }
+      if (correct === 1) {
+        return 'bg-positive';
+      }
+      if (correct === 2) {
+        return 'bg-yellow';
+      }
+      if (correct === 3) {
+        return 'bg-negative';
+      }
+    },
+    toLetter(index: number | string, max: number) {
+      if (!this.showLetters) {
+        return index;
+      }
+      if (index === 0) {
+        index = max;
+      }
+      if (Number(index) > 0) {
+        return String.fromCharCode(64 + Number(index));
+      }
+      return index;
+    },
+    sortedAnswers(answers: any[]) {
+      if (answers[0].answer === 0) {
+        answers.splice(answers.length - 3, 0, answers.shift());
+      }
+      return answers;
+    },
   },
 });
 </script>
@@ -87,25 +126,40 @@ export default defineComponent({
 .question-stat {
   width: 320px;
   margin: 16px;
+  overflow: hidden;
 }
 
 .answer-stat {
   white-space: nowrap;
+  padding-left: 4px;
+  position: relative;
 }
 
-.correct-0 {
-  background-color: orange;
+.answer-stat:hover:before {
+  content: ' ';
+  top: 0;
+  height: 24px;
+  position: absolute;
+  width: 320px;
+  background-color: rgba(180, 180, 180, 0.2) !important;
 }
 
-.correct-1 {
-  background-color: green;
+.answer-letter {
+  font-weight: bold;
+  padding: 0 4px;
+  width: 40px;
+  display: inline-block;
 }
 
-.correct-2 {
-  background-color: yellow;
+.answer-count {
+  text-align: right;
+  width: 40px;
+  display: inline-block;
 }
 
-.correct-3 {
-  background-color: red;
+.answer-percent {
+  text-align: right;
+  width: 80px;
+  display: inline-block;
 }
 </style>
